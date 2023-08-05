5bdd521bb99c02072b8930846ccf25b6adf74689
Andrew Kolbe <andrewkolbe@Andrews-MacBook-Pro.local>
2023-08-05 14:36:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
+connection = MySQLdb.connect(
+  host= os.getenv("HOST"),
+  user=os.getenv("USERNAME"),
+  passwd= os.getenv("PASSWORD"),
+  db= os.getenv("DATABASE"),
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
